9b3daf185e4a7dead645e2510ecf3c5bf73986fd
Giridharan Vel <giridharanvel@gmail.com>
2025-05-28 15:01:57 +0530

    add: files from forke repo

---
+++ demo.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+df = spark.read.table("samples.nyctaxi.trips")
+df.show(5)
+# COMMAND ----------
+
+print("Spark version:", spark.version)
